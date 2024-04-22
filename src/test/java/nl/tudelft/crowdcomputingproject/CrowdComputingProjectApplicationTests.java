53a2b0908eb115355b89674e2cd5f29780e13b3e
wjj710 <1691826460@qq.com>
2024-04-22 14:40:53 +0200

    create project

---
+++ src/test/java/nl/tudelft/crowdcomputingproject/CrowdComputingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.tudelft.crowdcomputingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowdComputingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
